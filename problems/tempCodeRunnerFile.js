e7026dbc11932cb7ed256e4974eb7a870117c3f4
Lama El-Sherbiny <lamasherb@gmail.com>
2021-04-14 18:30:20 -0400

    prob 5 solved

--- problems/tempCodeRunnerFile.js
+++ problems/tempCodeRunnerFile.js
@@ -1,23 +1,41 @@
+let selectiveMap = function(array, cb1, cb2) {
+    let newArray = [];
+    for (let i = 0; i< array.length; i++) {
+        let el= array[i];
+        if(cb1(el)) {
+            newArray.push(cb2(el));
+          
+        }else {
+            newArray.push(el)
 
-let multiMap = function(value,n,cb) {
-  // let newValue = 0
-  for(let i = 0; i < n; i++) {
-   let newValue = cb(value);
-    
-  }
-  return newValue;
+        }
+    }
+
+
+    return newArray;
 
 };
-let result1 = multiMap(7, 2, function (n) {
-  return n * 10;
-});
-console.log(result1); // 700
+function isEven(n) {
+    return n % 2 === 0;
+}
 
-let result2 = multiMap(7, 3, function (n) {
-  return n * 10;
-});
-console.log(result2); // 7000
+function isPositive(n) {
+    return n > 0;
+}
 
-let result3 = multiMap("hi", 5, function (s) {
-  return s + "!";
-});
+function square(n) {
+    return n * n;
+}
+
+function flipSign(n) {
+    return n * -1;
+}
+
+console.log(selectiveMap([8, 5, 10, 4], isEven, square));
+// [ 64, 5, 100, 16 ]
+
+console.log(selectiveMap([-10, 4, 7, 6, -2, -9], isEven, flipSign));
+// [ 10, -4, 7, -6, 2, -9 ]
+
+console.log(selectiveMap([-10, 4, 7, 6, -2, -9], isPositive, square));
+// [-10, 16, 49, 36, -2, -9]
