1ebaa09eebf7c643033de944c20d2a3bf9949ff8
Angeline <angiecummins11@gmail.com>
2021-04-14 17:39:10 -0500

    problem 06 solved

--- problems/06-reject.js
+++ problems/06-reject.js
@@ -21,12 +21,27 @@ let hasA = function(s) {
 console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth', 'height' ]
 *******************************************************************************/
 
-let reject = function() {
+let reject = function(array, cb) {
+    let allTheRejects = [];
+    array.forEach(function(el){
+        if (cb(el) === false){
+            allTheRejects.push(el);
+        }
 
+    });
+return allTheRejects;
 };
 
 
+let isEven = function(n) {
+    return n % 2 === 0;
+};
+console.log(reject([7, 8, 5, 6, 12, 11], isEven)); // [ 7, 5, 11 ]
 
+let hasA = function(s) {
+    return s.toLowerCase().includes('a');
+};
+console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth', 'height' ]
 
 
 
